f84735d5b7f68f55acb41f6094960f14ffd3e541
Julian <julianrecarte@gmail.com>
2024-05-21 17:09:10 +0200

    Exercise1.3

---
+++ Exercises1.jsx
@@ -0,0 +1,41 @@
+
+
+const Header = () => {
+  const course = 'Half Stack application development'
+  return(
+    <>
+      <h1>{course}</h1>
+    </>
+  )
+}
+
+const Content = ({part, exercises}) => {
+  return(
+    <>
+      <p>{part} {exercises}</p>
+    </>
+  )
+}
+
+const Total = ({e1, e2, e3}) =>{
+  const suma = parseInt(e1) + parseInt(e2) + parseInt(e3) 
+  return(
+    <>
+      <p>{suma}</p>
+    </>
+  )
+}
+
+const App = () => {    
+    return (
+      <> 
+        <Header />
+        <Content part = 'Fundamentals of React' exercises='10'/>
+        <Content part = 'Using props to pass data' exercises='7'/>
+        <Content part = 'State of a component' exercises='14'/>
+        <Total e1 = '10' e2 = '7' e3 = '14'/>
+      </>
+  )
+}
+
+export default App
